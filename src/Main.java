55db3e21772fe8b4c3cc7c2eb89a7e098451385b
Askar <askarhasja@gmail.com>
2026-01-22 13:47:46 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        String fullName = "Глеб Глебыч";
+
+
+        String group = "ТПП-24-22";
+
+
+        String program = "Технологии пищевого производства";
+
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("ОП/Специальность: " + program);
+    }
+}
