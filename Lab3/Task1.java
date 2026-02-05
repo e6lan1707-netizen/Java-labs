953d6ec9bec7aa057351eedd73d49f82964ed6c1
e6lan1707-netizen <e6lan1707@gmail.com>
2026-02-05 14:37:01 +0500

    Add files via upload

---
+++ Lab3/Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 10;
+        int c = 3;
+
+        int sum = a + b + c;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("Сумма = " + sum);
+    }
+}
